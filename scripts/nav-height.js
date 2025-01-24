8852fdf6851576eeca5495735d688d63c25246da
samlmcewan <samlmcewan@gmail.com>
2025-01-24 00:20:25 +0000

    hero

--- scripts/nav-height.js
+++ scripts/nav-height.js
@@ -13,3 +13,25 @@ if (window.matchMedia("(min-width: 1200px)").matches) {
         }
     }
 }
+
+// Darken the mobile nav on expanding 
+document.addEventListener("DOMContentLoaded", function () {
+    let count = 1;
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarBg = document.querySelector(".navbar-bg");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+
+    if (navbarToggler && navbarBg && navbarCollapse) {
+        navbarToggler.addEventListener("click", function () {
+            if (count % 2 === 0) {
+                // Menu is about to collapse
+                navbarBg.style.background = "linear-gradient(98deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 1) 80%)";
+            } else {
+                // Menu is about to expand
+                navbarBg.style.background = "linear-gradient(98deg, rgba(0, 0, 0, 0.85) 0%, rgba(0, 0, 0, 1) 80%)";
+            }
+            count++;
+        });
+    }
+});
+
